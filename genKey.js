a95d41a94232dc052a7a415d42f190ac07a50b8d
Luis Henrique Souza <mora0199@algonquinlive.com>
2019-05-02 19:50:45 -0400

    fetch error fixed

--- genKey.js
+++ genKey.js
@@ -4,5 +4,5 @@ console.log("OLD APP_JWTKEY value: ", process.env.APP_JWTKEY)
 var newKey = [...Array(30)].map(e => ((Math.random() * 36) | 0).toString(36)).join('')
 process.env.APP_JWTKEY = newKey
 
-console.log("NEW APP_JWTKEY value: ", process.env.APP_JWTKEY, "already set as ENV variable!")
+console.log("Use EXPORT APP_JWTKEY=" + process.env.APP_JWTKEY, "to set the new value to memory...")
 console.log()
