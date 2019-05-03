04242f08f1f2b83be8a31cfc0936ee836da7a70f
Luis Henrique Souza <mora0199@algonquinlive.com>
2019-05-02 22:30:01 -0400

    issue

--- middleware/sanitizeBody.js
+++ middleware/sanitizeBody.js
@@ -1,5 +1,6 @@
 const debug = require('debug')('final-mora0199-call0099:db')
 const xss = require('xss')
+const logger = require('../startup/logger')
 
 const sanitize = sourceString => {
   return xss(sourceString, {
@@ -14,6 +15,7 @@ const stripTags = payload => {
   for (let key in attributes) {
     if (attributes[key] instanceof Array) {
       debug('Recurse array', attributes[key])
+      logger.log('info', attributes[key])
       attributes[key] = attributes[key].map(element => {
         return typeof element === 'string'
           ? sanitize(element)
@@ -21,9 +23,11 @@ const stripTags = payload => {
       })
     } else if (attributes[key] instanceof Object) {
       debug('Recurse object', attributes[key])
+      logger.log('info', attributes[key])
       attributes[key] = stripTags(attributes[key])
     } else {
       debug('Recurse general', key, attributes[key])
+      logger.log('info', attributes[key])
       attributes[key] = sanitize(attributes[key])
     }
   }
@@ -32,10 +36,13 @@ const stripTags = payload => {
 
 module.exports = (req, res, next) => {
   debug({ body: req.body })
+  logger.log('info', req.body)
   const { id, _id, ...attributes } = req.body
   debug({ attributes })
+  logger.log('info', attributes)
   const sanitizedBody = stripTags(attributes)
   debug({ sanitizedBody })
+  logger.log('info', sanitizedBody)
   req.sanitizedBody = sanitizedBody
   next()
 }
